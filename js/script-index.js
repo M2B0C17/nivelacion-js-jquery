a7a973be6680c303f788e19856200dc8e3bca280
Macarena Baltra <macarena.bc2011@gmail.com>
2017-07-10 22:30:58 -0400

    Nivel 5 - Intento Fallido

--- js/script-index.js
+++ js/script-index.js
@@ -48,14 +48,27 @@ function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
 }
 
-
+/* NOTA IMPORTANTE!!!  
+ERROR: La funcion anterior que recorre el arreglo y todo, lo hacia con mi parametro (el), que es el elemento,
+al retornar o llamar a la siguiente funcion, quedaba inutil, ya que, sin pasarle el elemento que recorre y 
+filtra (true), se corta la cadena y no se une a la siguiente función. Por lo tanto, el valor dejaba de 
+existir.
+*/
 
 /*
 * Función que se encarga de pintar todas las actividades
 */
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
+	var filtro2 = activitiesArray.length; // una vez que se cumple esta condicion se recorre
+		if(filtro > 0){			
+			var array = activitiesArray.forEach(function(el2){ // forEach para que recorra
+				return renderActivity(el2); // llamamos a la siguiente funcion 
+				$('.wrapper-message').remove();
+			});
+	}
 }
+
 
 /*
 * Función que se encarga de pintar una actividad
